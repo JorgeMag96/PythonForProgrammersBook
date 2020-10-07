869f412455c49c6b339ba28120298a89a8191f78
Jorge Villarreal <jorge.vi96@gmail.com>
2020-10-07 08:28:39 -0500

    Jupyter Notebooks and Dictionaries

--- introductory_AI_cases/dynamic_visualizations/RollDieDynamic.py
+++ introductory_AI_cases/dynamic_visualizations/RollDieDynamic.py
@@ -1,6 +1,6 @@
-'''
-    Mathplotlib animation module's FuncAnimation function,
+"""
+    Matplotlib animation module's FuncAnimation function,
     which updates the bar plot dynamically. You'll se the bars,
     die frequencies and percentages "come alive", updating
     continuously as the roll occurs.
-'''
+"""
