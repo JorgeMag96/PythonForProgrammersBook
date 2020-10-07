f26e59419feb36b026d002ef7da5caff5f21113b
Jorge Villarreal <jorge.vi96@gmail.com>
2020-10-07 06:29:26 -0500

    Initial commit

---
+++ dictionaries_and_sets/dynamic_visualizations/RollDieDynamic.py
@@ -0,0 +1,6 @@
+'''
+    Mathplotlib animation module's FuncAnimation function,
+    which updates the bar plot dynamically. You'll se the bars,
+    die frequencies and percentages "come alive", updating
+    continuously as the roll occurs.
+'''
